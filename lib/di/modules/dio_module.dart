4d2712e267eef1de362ec1695ec231bd95b8a09b
Abdullah Alwardi <71692557+abdullahalwardi@users.noreply.github.com>
2023-03-16 13:57:16 +0300

    logout functionality

--- lib/di/modules/dio_module.dart
+++ lib/di/modules/dio_module.dart
@@ -1,16 +1,18 @@
 import 'package:app/constants/api_document.dart';
 import 'package:app/data/client/interceptors/authenticator.dart';
+import 'package:app/router/app_router.dart';
 import 'package:app/src/auth/authentication_provider.dart';
 import 'package:app/utils/snackbar.dart';
 import 'package:awesome_dio_interceptor/awesome_dio_interceptor.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 final dioProvider = Provider<Dio>(
-    (ref) => _dioProvider(ref.read(authenticationProvider.notifier)));
+    (ref) => _dioProvider(ref.read(authenticationProvider.notifier),ref.read(goRouterProvider)));
 
-  Dio _dioProvider(AuthenticationNotifier authenticationNotifier) {
+  Dio _dioProvider(AuthenticationNotifier authenticationNotifier, GoRouter router) {
   final dio = Dio();
   dio
     ..options.baseUrl = ApiDocument.baseUrl
@@ -25,10 +27,12 @@ final dioProvider = Provider<Dio>(
     ..interceptors
         .add(InterceptorsWrapper(onError: (DioError e, handler) async {
       if (e.response?.statusCode == 401) {
+        authenticationNotifier.logout();
+        router.pushReplacement(RoutesDocument.home);
         final message =
         e.response!.data['message'];
         Utils.showErrorSnackBar(message);
-        authenticationNotifier.logout();
+        
       }
       handler.next(e);
     }));
