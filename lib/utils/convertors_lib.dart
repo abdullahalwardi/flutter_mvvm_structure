a0f2286ef9b7eb6d8adcdb82694117449c758580
Abdullah Alwardi <71692557+abdullahalwardi@users.noreply.github.com>
2023-04-06 22:28:06 +0300

    add common convertors lib

---
+++ lib/utils/convertors_lib.dart
@@ -0,0 +1,5 @@
+export 'package:app/data/client/services/convertors/nullable_date_time_convertor.dart';
+export 'package:app/data/client/services/convertors/nullable_phone_number_convertor.dart';
+export 'package:app/data/client/services/convertors/nullable_string_convertor.dart';
+export 'package:app/data/client/services/convertors/nullable_uri_convertor.dart';
+export 'package:app/data/client/services/convertors/xfile_multipart_convertor.dart';
