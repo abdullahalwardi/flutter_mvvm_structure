82a312f0aac4a3d6ddf6fe5698d423389b6c563b
Abdullah Alwardi <71692557+abdullahalwardi@users.noreply.github.com>
2023-04-09 21:17:04 +0300

    common lib

--- lib/common_lib.dart
+++ lib/common_lib.dart
@@ -1,2 +1,9 @@
-export 'theme/l10n.dart';
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
+// export 'package:app/theme/app_colors.dart';
+export 'package:app/theme/l10n.dart';
+export 'package:app/utils/constants/insets.dart';
+export 'package:flutter_riverpod/flutter_riverpod.dart';
+export 'package:gap/gap.dart';
+// export 'package:app/constants/assets.dart';
+export 'package:app/utils/constants/border_size.dart';
+export 'package:app/utils/extensions.dart';
