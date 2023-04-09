7bf4ff888dede2bcd920979f6c8d18d2f6014d76
Abdullah Alwardi <71692557+abdullahalwardi@users.noreply.github.com>
2023-04-09 14:36:59 +0300

    update on local services

--- lib/data/client/services/local_services.dart
+++ lib/data/client/services/local_services.dart
@@ -1,6 +1,8 @@
 
 import 'package:app/data/client/services/callback.dart';
+import 'package:flutter/foundation.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 Future<void> fetchPage<T>(
   {
@@ -24,4 +26,34 @@ Future<void> fetchPage<T>(
   } catch (error) {
     pagingController.error = error;
   }
+}
+
+Future<void> launchNotNullUrlString(String? url, {VoidCallback? orElse}) async {
+  if (url != null && (url.contains("https") || url.contains("http"))) {
+    launchUrlString(url, mode: LaunchMode.externalApplication);
+  } else {
+    orElse?.call();
+  }
+}
+
+Future<void> openCall(String phoneNumber) async {
+  try {
+    await launchUrlString("tel:$phoneNumber");
+  } catch (_) {
+    if (kDebugMode) print("error: tel:$phoneNumber");
+  }
+}
+
+Future<void> openMail(String mail) async {
+  try {
+    await launchUrlString("mailto:$mail");
+  } catch (_) {
+    if (kDebugMode) print("error: mailto:$mail");
+  }
+}
+
+Future<void> openSMSMessage(String phoneNumber, {String? message}) async {
+  var uri = 'sms:$phoneNumber';
+  if (message != null) uri += "?body=$message";
+  if (await canLaunchUrlString(uri)) await launchUrlString(uri);
 }
