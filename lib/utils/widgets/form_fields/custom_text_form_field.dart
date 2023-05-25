7a1372d1fb5a17128201cb42b4fb626414eafb7e
Abdullah Alwardi <71692557+abdullahalwardi@users.noreply.github.com>
2023-05-25 20:15:58 +0300

    update custom text form field

--- lib/utils/widgets/form_fields/custom_text_form_field.dart
+++ lib/utils/widgets/form_fields/custom_text_form_field.dart
@@ -1,36 +1,52 @@
+import 'package:app/utils/extensions.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-   const CustomTextFormField({
+  const CustomTextFormField({
     super.key,
     required this.controller,
-    required this.label,
     required this.hintText,
-    required this.suffixIcon,
+    this.suffixIcon,
+    this.prefixIcon,
     this.validator,
+    this.fillColor,
+    this.onChanged,
+    this.maxLines = 1,
+    this.maxLength,
+    this.obscureText = false,
   });
 
   final TextEditingController controller;
-  final String label;
   final String hintText;
-  final Widget suffixIcon;
+  final Widget? suffixIcon, prefixIcon;
+  final Color? fillColor;
   final String? Function(String?)? validator;
+  final ValueChanged<String?>? onChanged;
+  final int? maxLines, maxLength;
+  final bool obscureText;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(
-        label: Text(label),
+        fillColor: fillColor,
         hintText: hintText,
+        hintStyle: context.textTheme.bodyLarge?.copyWith(
+          color: context.colorScheme.outline,
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+        ),
+        prefixIcon: prefixIcon,
         suffixIcon: suffixIcon,
       ),
       validator: validator,
       autovalidateMode: AutovalidateMode.onUserInteraction,
+      maxLines: maxLines,
+      minLines: maxLines,
+      onChanged: onChanged,
+      maxLength: maxLength,
+      obscureText: obscureText,
     );
   }
 }
-
-
-
-
