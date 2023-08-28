c06f1b395291060e1c08ad9be7608a8100d5767e
Abdullah Alwardi <71692557+abdullahalwardi@users.noreply.github.com>
2023-08-28 20:57:07 +0300

    fix on phone number form field

--- lib/utils/widgets/form_fields/phone_number_form_field.dart
+++ lib/utils/widgets/form_fields/phone_number_form_field.dart
@@ -5,8 +5,9 @@ import 'package:flutter/material.dart';
 class PhoneNumberFormField extends StatelessWidget {
   const PhoneNumberFormField({super.key, required this.controller, required this.onChanged, required this.onCountryCodeChanged});
 
-  final ValueChanged onChanged, onCountryCodeChanged;
+  final ValueChanged<String?> onChanged;
   final TextEditingController controller;
+  final ValueChanged<CountryCode> onCountryCodeChanged;
 
   @override
   Widget build(BuildContext context) {
