fc30865bff8304eae325b14f5a2ecb618702d421
Abdullah Alwardi <71692557+abdullahalwardi@users.noreply.github.com>
2023-09-09 15:29:30 +0300

    update on cache manager

--- lib/data/services/cache_manager.dart
+++ lib/data/services/cache_manager.dart
@@ -1,34 +1,37 @@
+// import 'package:flutter/foundation.dart';
 // import 'package:path_provider/path_provider.dart';
 // import 'dart:io';
 // import 'package:isar/isar.dart';
 // import 'package:app/data/models/post_model.dart';
+// import 'package:app/data/services/file_plugin.dart';
 
 // class CacheManager {
-//   static const int maxCacheSize = 1000 * 1024 * 1024; // 500 MB
+//   static const int maxCacheSize = 1000 * 1024 * 1024; // 1000 MB
 
 //   Future<void> clearCacheAndDatabase(Isar isar) async {
-//     final cacheDir = await getTemporaryDirectory();
+//     final cacheDir = await FilePlugin().getAppTemporaryDirectory();
 //     int currentCacheSize = 0;
 
 //     // Get a list of files in the cache directory
-//     final cacheFiles = cacheDir.listSync();
+//     final cacheFiles = cacheDir?.listSync();
 
-//     for (final file in cacheFiles) {
-//       if (file is File) {
-//         currentCacheSize += file.lengthSync();
-//         if (currentCacheSize > maxCacheSize) {
-//           // Delete the oldest files until cache size is within limit
-//           await file.delete();
+//     if (cacheFiles != null) {
+//       for (final file in cacheFiles) {
+//         if (file is File) {
+//           currentCacheSize += file.lengthSync();
+//           if (currentCacheSize > maxCacheSize) {
+//             // Delete the oldest files until cache size is within limit
+//             await file.delete();
 
-//           // Also remove the corresponding video data from Isar
-//           final videoId = await getVideoIdFromFile(file);
-//           if (videoId != null) {
-//             final video = await isar.postSchemes
-//                 .where()
-//                 .isarIdEqualTo(videoId)
-//                 .findFirst();
-//             if (video != null) {
-//               await isar.postSchemes.delete(video.isarId);
+//             // Also remove the corresponding video data from Isar
+//             final videoId = await getVideoIdFromFile(file);
+//             if (videoId != null) {
+//               final video =
+//                   await isar.postSchemes.where().idEqualTo(videoId).findFirst();
+//               if (video != null && video.id != null) {
+//                 await isar.postSchemes.delete(video.id!);
+//                 debugPrint("video deleted from cache: ${video.id}");
+//               }
 //             }
 //           }
 //         }
