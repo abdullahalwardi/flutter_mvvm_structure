1584e363f9e9861e2245fcb094e0726f09e10e79
Abdullah Alwardi <71692557+abdullahalwardi@users.noreply.github.com>
2023-11-22 22:30:50 +0300

    notification provider

---
+++ lib/data/providers/notifications_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'notifications_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$sendNotifiactionByTopicHash() =>
+    r'4793b800f14bb77c62f68d0826e51abf1fed75ea';
+
+/// See also [SendNotifiactionByTopic].
+@ProviderFor(SendNotifiactionByTopic)
+final sendNotifiactionByTopicProvider = AutoDisposeAsyncNotifierProvider<
+    SendNotifiactionByTopic, AsyncX<dynamic>>.internal(
+  SendNotifiactionByTopic.new,
+  name: r'sendNotifiactionByTopicProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$sendNotifiactionByTopicHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SendNotifiactionByTopic = AutoDisposeAsyncNotifier<AsyncX<dynamic>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
