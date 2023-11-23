2fac6526d8eafb599a47cd50f5dc094eba3bc7c9
Abdullah Alwardi <71692557+abdullahalwardi@users.noreply.github.com>
2023-11-23 19:44:11 +0300

    network image with loader

---
+++ lib/utils/widgets/network_image_with_loader.dart
@@ -0,0 +1,42 @@
+import 'package:app/utils/constants/sizes.dart';
+import 'package:app/utils/widgets/skelton.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class NetworkImageWithLoader extends StatelessWidget {
+  final BoxFit fit;
+
+  const NetworkImageWithLoader(
+    this.src, {
+    Key? key,
+    this.fit = BoxFit.cover,
+    this.radius = BorderSize.small,
+  }) : super(key: key);
+
+  final String src;
+  final double radius;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.all(Radius.circular(radius)),
+      child: CachedNetworkImage(
+        fit: fit,
+        imageUrl: src,
+        imageBuilder: (context, imageProvider) => Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: imageProvider,
+              fit: fit,
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(radius),
+            ),
+          ),
+        ),
+        placeholder: (context, url) => const CustomSkeleton(),
+        errorWidget: (context, url, error) => const Icon(Icons.error),
+      ),
+    );
+  }
+}
---
+++ lib/utils/widgets/skelton.dart
@@ -0,0 +1,43 @@
+import 'package:app/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+
+class CustomSkeleton extends StatelessWidget {
+  const CustomSkeleton({Key? key, this.height, this.width, this.layer = 1})
+      : super(key: key);
+
+  final double? height, width;
+  final int layer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      padding: const EdgeInsets.all(Insets.medium / 2),
+      decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.04 * layer),
+          borderRadius:
+              const BorderRadius.all(Radius.circular(BorderSize.small))),
+    );
+  }
+}
+
+class CircleSkeleton extends StatelessWidget {
+  const CircleSkeleton({Key? key, this.size = 24}) : super(key: key);
+
+  final double? size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size,
+      width: size,
+      // padding: const EdgeInsets.all(defaultPadding),
+      decoration: BoxDecoration(
+        color: Theme.of(context).primaryColor.withOpacity(0.04),
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+}
