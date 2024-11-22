1bb819a0f56a134122b460e60dc58651911a2548
Abdullah Alwardi <71692557+abdullahalwardi@users.noreply.github.com>
2024-11-22 03:49:01 +0300

    upgrade

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,7 @@ import file_selector_macos
 import flutter_kurdish_localization
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
